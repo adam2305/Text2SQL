6de6a9d9a373b7c8d198ee48859ad6c24846788d
Jeanne Malecot <jeanne.malecot@telecom-paris.fr>
2025-01-05 23:39:26 +0100

    add metrics file

--- metrics.py
+++ metrics.py
@@ -1,4 +1,15 @@
+import numpy as np
+
 
 #exact match accuracy
-def EMA(y_true, y_pred):
-    return np.mean(y_true == y_pred)
+def EMA(predicts: list, gold_answers: list) -> float:
+    sum = np.sum([y_true == y_pred for y_true, y_pred in zip(gold_answers, predicts)])
+    return sum / len(gold_answers)
+
+#execution accuracy
+def EA(predicts: list, gold_answers: list) -> float:
+    """
+    Check the ratio of predictions that gave the same execution result as the gold answer
+    """
+    sum = np.sum([y_true == y_pred for y_true, y_pred in zip(gold_answers, predicts)])
+    return sum / len(gold_answers)
